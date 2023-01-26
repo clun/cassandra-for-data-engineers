50ca8e41f51514d59b23cd3c783526658fda76b1
clun <cedrick.lunven@gmail.com>
2023-01-26 01:08:40 +0100

    split components

--- 2-storage-in-cassandra/labs/hotel-reviews-ui/src/components/SectionSlider.js
+++ 2-storage-in-cassandra/labs/hotel-reviews-ui/src/components/SectionSlider.js
@@ -18,31 +18,33 @@ const SectionSlider = () => {
     useEffect(() => {
         fetchLocations()
     }, [])
- 
+
     return (
         <section className="slider">
             <div id="slider" className="flexslider">
                 <ul className="slides">
-                {locations.map((location, index) => <SlideBig key={index} location={location} />)}
-
+                    {locations && locations.map((location, index) => <SlideBig key={index} location={location} />)}
                 </ul>
                 <div id="icon_drag_mobile"></div>
             </div>
             <div id="carousel_slider_wp">
                 <div id="carousel_slider" className="flexslider">
-                    <ul className="slides">
-                    {locations.map((location, index) => <SlideSmall key={index} location={location} />)}
-                       
+                  <div className="flex-viewport" style={{overflow: "hidden", position: "relative"}}>
+                    <ul className="slides"  style={{width: "1000%",transitionDuration: "0s",transform: "translate3d(0px, 0px, 0px)"}} >
+                        {locations && locations.map((location, index) => <SlideSmall key={index} location={location} />)}
                     </ul>
+                  </div>
+                    <ul className="flex-direction-nav">
+                    <li className="flex-nav-prev">
+                        <a className="flex-prev" href="#" tabIndex="-1"></a>
+                    </li>
+                    <li className="flex-nav-next">
+                        <a className="flex-next" href="#" tabIndex="-1">
+                        </a>
+                    </li>
+                </ul>
                 </div>
-                <ul className="flex-direction-nav">
-                        <li className="flex-nav-prev">
-                            <a className="flex-prev flex-disabled" href="#" tabindex="-1"></a>
-                        </li>
-                        <li className="flex-nav-next">
-                            <a className="flex-next flex-disabled" href="#" tabindex="-1">
-                            </a>
-                        </li></ul>
+                
             </div>
         </section>
     )
